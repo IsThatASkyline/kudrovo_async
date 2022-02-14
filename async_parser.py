fea3990dd3b0c26b01ff0733a76db69dd7ce5dcb
artklk12 <67318020+artklk12@users.noreply.github.com>
2022-02-14 23:30:26 +0300

    Update async_parser.py

--- async_parser.py
+++ async_parser.py
@@ -3,9 +3,13 @@ from pymongo import MongoClient
 import time
 import asyncio
 import aiohttp
+
+
 start_time = time.time()
 
+all_links = []
 cards_data = []
+
 
 def get_database():
 
@@ -31,39 +35,35 @@ async def get_data(session, link_product):
     async with session.get(url=url, headers=headers) as response:
         src = await response.text()
         soup = BeautifulSoup(src, "lxml")
-        
+
         try:
             title = soup.find('h1', class_='a10a3f92e9--title--UEAG3').text
-        except:
+        except Exception:
             title = "Нет названия"
-
         try:
             prise = soup.find('span', class_='a10a3f92e9--price_value--lqIK0').text
-        except:
+        except Exception:
             prise = "Нет цены"
-
         try:
             address = soup.find('address', class_='a10a3f92e9--address--F06X3').text.rstrip('На карте')
-        except:
+        except Exception:
             address = "Нет адреса"
-
         try:
             opisanie = soup.find('p', class_='a10a3f92e9--description-text--YNzWU').text
-        except:
+        except Exception:
             opisanie = "Нет описания"
-
         try:
-            image = soup.find('div', class_='a10a3f92e9--photo_gallery_container--OS_kt').find("div").find("span").find("span").get("content")
+            image = soup.find('div', class_='a10a3f92e9--photo_gallery_container--OS_kt').find("div").find("span").find(
+                "span").get("content")
         except Exception:
             image = "Нет картинки"
-
         cards_data.append(
             {
                 "Название": title,
                 "Цена": prise.replace(" ", " "),
                 "Адрес": address,
                 "Описание": opisanie.replace("\n", ""),
-                "Ссылка": link,
+                "Ссылка": link_product,
                 "Картинка": image
             }
         )
@@ -71,8 +71,7 @@ async def get_data(session, link_product):
 
 async def get_links(session, page):
 
-    url=f"https://spb.cian.ru/cat.php?deal_type=rent&engine_version=2&location%5B0%5D=244909&offer_type=flat&p={page}&room1=1&room2=1&type=4",
-
+    url = f"https://spb.cian.ru/cat.php?deal_type=rent&engine_version=2&location%5B0%5D=244909&offer_type=flat&p={page}&room1=1&room2=1&type=4"
     headers = {
         "accept": "*/*",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
@@ -95,20 +94,18 @@ async def get_links(session, page):
 async def gather_data():
 
     url = "https://spb.cian.ru/cat.php?deal_type=rent&engine_version=2&location%5B0%5D=244909&offer_type=flat&room1=1&room2=1&type=4"
-
     headers = {
         "accept": "*/*",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     }
-    
     async with aiohttp.ClientSession() as session:
         r = await session.get(url=url, headers=headers)
         soup = BeautifulSoup(await r.text(), "lxml")
         pages_count = int(soup.find("div", class_="_93444fe79c--wrapper--bKcEk").find_all("li")[-1].text)
 
         tasks = []
-        for page in range(1, pages_count + 1):
 
+        for page in range(1, pages_count + 1):
             task = asyncio.create_task(get_links(session, page))
             tasks.append(task)
 
@@ -116,19 +113,22 @@ async def gather_data():
 
 def main():
     asyncio.run(gather_data())
-
+    #
     # with open(f"cards_data_asyncio.json", "w", encoding="utf-8") as file:
     #     json.dump(cards_data, file, indent=4, ensure_ascii=False)
-
-    # finish_time = time.time() - start_time()
+    #
+    # finish_time = time.time() - start_time
+    # print(f"Всего квартир: {len(cards_data)}")
     # print(f"Затраченное время на работу скрипта: {finish_time}")
+    #
+    # with open(f"cards_data_asyncio.json", "r", encoding="utf-8") as file:
+    #     cards_data = json.load(file)
+
     db = get_database()
     db.user_1_items.drop()
     col = db["user_1_items"]
     for card in cards_data:
         col.insert_one(card)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
